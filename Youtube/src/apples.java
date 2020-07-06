021e0f41ae9500de6d2f8b91de337a34b3e54fc7
m54b30 <62607824+m54b30@users.noreply.github.com>
2020-07-06 22:31:09 +0300

    07-06

--- Youtube/src/apples.java
+++ Youtube/src/apples.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 class apples{
 	
@@ -112,12 +113,12 @@ class apples{
 //		}
 //	}
 	
-//	public static void main(String args[]) {
+//	public static void main(String[] args) {
 //		tuna tunaObject = new tuna();
 //		tunaObject.simpleMessage();
 //	}
 	
-//	public static void main(String args[]) {
+//	public static void main(String[] args) {
 //		Scanner input = new Scanner(System.in);
 //		tuna tunaObject = new tuna();
 //		
@@ -127,7 +128,7 @@ class apples{
 //		tunaObject.simpleMessage(name);
 //	}
 	
-//	public static void main(String args[]) {
+//	public static void main(String[] args) {
 //		Scanner input = new Scanner(System.in);
 //		tuna tunaObject = new tuna();
 //		
@@ -137,10 +138,142 @@ class apples{
 //		tunaObject.saying();
 //	}
 	
-	public static void main(String args[]) {
-		tuna tunaObject = new tuna("Ariana");
-		tuna tunaObject2 = new tuna("Halsey");
-		tunaObject.saying();
-		tunaObject2.saying();
+//	public static void main(String[] args) {
+//		tuna tunaObject = new tuna("Ariana");
+//		tuna tunaObject2 = new tuna("Halsey");
+//		tunaObject.saying();
+//		tunaObject2.saying();
+//	}
+	
+//	public static void main(String[] args) {
+//		int age = 60;
+//		
+//		if (age < 50) {
+//			System.out.println("You are young");
+//		}else{
+//			System.out.println("You are old");
+//			if(age > 75) {
+//				System.out.println("You are really old");
+//			}else {
+//				System.out.println("Dont worry you arent really that old");
+//			}
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//		int age = 45;
+//		
+//		if(age >= 60) 
+//			System.out.println("You are senior citizen");
+//		else if( age >= 50)
+//			System.out.println("You are in your 50s");
+//		else if( age >= 40)
+//			System.out.println("You are in your 40s");
+//		else
+//			System.out.println("You are young");
+//	}
+	
+//	public static void main(String[] args) {
+//		int age = 21;
+//		
+//		System.out.println(age > 50 ? "You are old" : "You are young");
+//
+//	}
+	
+//	public static void main(String[] args) {
+//		Scanner input = new Scanner(System.in);
+//		int total = 0;
+//		int grade;
+//		int average;
+//		int counter = 0;
+//		
+//		while(counter < 10){
+//			grade = input.nextInt();
+//			total = total + grade;
+//			counter++;
+//		}
+//		average = total/10;
+//		System.out.println("Your average is " + average);
+//	}
+	
+//	public static void main(String[] args) {
+//		for(int counter = 0; counter <= 30; counter+=3) {
+//			System.out.println(counter);
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//		double amount;
+//		double principal = 10000;
+//		double rate = .01;
+//		
+//		for(int day = 1; day<=20; day++) {
+//			amount = principal*Math.pow(1 + rate, day);
+//			System.out.println(day + "  " + amount);
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//		int counter = 0;
+//		
+//		do{
+//			System.out.println(counter);
+//			counter++;
+//		}while(counter <=10);
+//	}
+	
+//	public static void main(String[] args) {
+//		System.out.println(Math.sqrt(9));
+//	}
+	
+//	public static void main(String[] args) {
+//		Random dice = new Random();
+//		int number;
+//		
+//		for(int counter = 1; counter<=10; counter++) {
+//			number = 1 + dice.nextInt(6);
+//			System.out.println(number + " ");
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//	
+//		int bucky[] = {2,5,6,10,8,9,11,123};
+//		
+//		System.out.println(bucky[5]);
+//	}
+	
+//	public static void main(String[] args) {
+//		
+//		System.out.println("Index\tValue");
+//		int bucky[] = {22,33,44,55,123,456,789};
+//		
+//		for(int counter = 0; counter < bucky.length; counter++) {
+//			System.out.println(counter + "\t" + bucky[counter]);
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//		int bucky[] = {21,45,56,89,45};
+//		int sum = 0;
+//		
+//		for(int counter = 0; counter < bucky.length; counter++) {
+//			sum+=bucky[counter];
+//		}
+//		System.out.println("The sum of these number is " + sum);
+//	}
+	
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int freq[] = new int[7];
+		
+		for(int roll = 1; roll < 1000; roll++) {
+			++freq[1 + rand.nextInt(6)];
+		}
+		System.out.println("Face\tFrequency");
+		
+		for(int face = 1; face < freq.length; face++) {
+			System.out.println(face+"\t"+freq[face]);
+		}
 	}
 }
--- Youtube/src/tuna.java
+++ Youtube/src/tuna.java
@@ -1,25 +1,27 @@
 
 public class tuna {
-//	public void simpleMessage() {
-//		System.out.println("This is another class");
-//	}
+	public void simpleMessage() {
+		System.out.println("This is another class");
+	}
 	
-//	public void simpleMessage(String name) {
-//		System.out.println("Hello " + name);
-//	}
-	
+	public void simpleMessage(String name) {
+		System.out.println("Hello " + name);
+	}
 	
 	private String girlName;
+	
 	public tuna(String name) {
 		girlName = name;
 	}
-	
+
 	public void setName(String name) {
 		girlName = name;
 	}
+	
 	public String getName(){
 		return girlName;
 	}
+	
 	public void saying() {
 		System.out.printf("Your first girlfriend was %s", getName());
 	}
